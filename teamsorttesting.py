a7bbd69bb8b3d91620e537f5452d656398b0f084
Horep <horep245@gmail.com>
2020-05-21 03:21:46 +0100

    Added two additional methods of sorting
    Added:
    AB-ABBA-ABBA 
    AB-BABABABA
    methods of sorting.

--- teamsorttesting.py
+++ teamsorttesting.py
@@ -26,14 +26,31 @@ def GenTeamsFairness(P):
     B_Val = TeamValue(B_dum) / 5
     A_dum = sorted(list(A_dum), reverse=True, key=getKey)
     B_dum = sorted(list(B_dum), reverse=True, key=getKey)
+
     sNap_A = [P[1], P[3], P[5], P[7], P[9]]
     sNap_B = [P[0], P[2], P[4], P[6], P[8]]
     Val_As = TeamValue(sNap_A) / 5
     Val_Bs = TeamValue(sNap_B) / 5
-    return Fairness(A_Val, B_Val), Fairness(Val_As, Val_Bs)
+
+    TaF_A = [P[0], P[2], P[5], P[6], P[9]]
+    TaF_B = [P[1], P[3], P[4], P[7], P[8]]
+    Val_At = TeamValue(TaF_A) / 5
+    Val_Bt = TeamValue(TaF_B) / 5
+
+    Ville_A = [P[0], P[3], P[5], P[7], P[9]]
+    Ville_B = [P[1], P[2], P[4], P[6], P[8]]
+    Val_Av = TeamValue(Ville_A) / 5
+    Val_Bv = TeamValue(Ville_B) / 5
+
+    MyFairness = Fairness(A_Val, B_Val)
+    sNapFairness = Fairness(Val_As, Val_Bs)
+    TaF_Fairness = Fairness(Val_At, Val_Bt)
+    Ville_Fairness = Fairness(Val_Av, Val_Bv)
+
+    return MyFairness, sNapFairness, TaF_Fairness, Ville_Fairness
 
 
-iterations = 100
+iterations = 500000
 t = []
 for i in range(0, iterations):
     t.append(GenTeamsFairness(RandomTeam()))
